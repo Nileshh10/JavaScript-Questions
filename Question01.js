d90a0de43bc12628aa693f5fd55e79ce224ed94c
Nileshh10 <duhan.nilesh@gmail.com>
2022-11-15 21:46:53 +0530

    first commit

---
+++ Question01.js
@@ -0,0 +1,18 @@
+//1. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with there type.
+
+ let myName = "Nilesh Kumar";   
+ let userLoggedIn = true;
+ let str;
+ let me = null;
+
+ console.log(myName);
+ console.log(typeof myName);
+
+ console.log(userLoggedIn);
+ console.log(typeof userLoggedIn);
+
+ console.log(str);
+ console.log(typeof str);
+
+ console.log(me);
+ console.log(typeof me);
