4d59907a7ec2331e1b461faaffb49d5d2e28c094
Nileshh10 <duhan.nilesh@gmail.com>
2022-11-16 20:43:54 +0530

    first commit

---
+++ Question02.js
@@ -0,0 +1,15 @@
+//2. Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.
+
+ let firstName = "Nilesh";
+ let lastName = "Kumar";
+ let maritalStatus = "Single";
+ let country = "India";
+ let age = 22;
+ 
+ console.log(`
+ My first Name is : ${firstName}
+ My last Name is : ${lastName}
+ Marital Status is : ${maritalStatus}
+ My country is : ${country}
+ My age is : ${age}
+ `);
---
+++ Question03.js
@@ -0,0 +1,4 @@
+//3. Decalre a varibale and assign value to it and change all the string characters to capital letters using toUpperCase() method.
+
+ let course = "full stack javascript web development";
+ console.log(course.toUpperCase());
