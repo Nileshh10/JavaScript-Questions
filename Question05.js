ef01555ae586b3026a6ba3108f047b99174aa6ce
Nileshh10 <duhan.nilesh@gmail.com>
2022-11-17 21:02:24 +0530

    first commit

---
+++ Question05.js
@@ -0,0 +1,5 @@
+//5. Split the string into an array using split() method
+
+ let para = "when a program or whatever we are making is capable taking decisions is called programming";
+ console.log(para.split(" "));
+ console.log(para.split(""));   //each character separate 
---
+++ Question09.js
@@ -0,0 +1,5 @@
+//9. Use trim() to remove any trailing whitespace at the beginning and the end of a string.
+
+ let str = '    Removes whitespace from both ends of a string and returns a new string, without modifying the original string    ';
+  console.log(str);     //original
+  console.log(str.trim());    //trim
