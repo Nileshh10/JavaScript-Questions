ea3039e399c97f1fd09f3c7eea358731193dee30
Nileshh10 <duhan.nilesh@gmail.com>
2022-12-06 21:04:42 +0530

    answer updated

--- Question04.js
+++ Question04.js
@@ -1,1 +1,7 @@
 //4. Check if the string contains a word Script using includes() method.
+
+let sampleString = 'Web development is the work involved in developing a website for the Internet or an intranet. Web development can range from developing a simple single static page of plain text to complex web applications, electronic businesses, and social network services.';
+
+const word = 'development';
+
+console.log(`Does the string contain the word : ${word} ? ${sampleString.includes(word) ? 'Yes' : 'No'}`);
